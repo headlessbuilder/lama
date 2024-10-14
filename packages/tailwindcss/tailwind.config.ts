f35ce78ff170e2f8792279f0d07d699138e2d491
Bernardo Forcillo <bernardoforcillo@outlook.com>
2024-10-14 21:23:40 +0200

    chore: added internal packages and github configs

---
+++ packages/tailwindcss/tailwind.config.ts
@@ -0,0 +1,10 @@
+import forms from "@tailwindcss/forms";
+import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography";
+
+const config: Omit<Config, "content"> = {
+  theme: {},
+  plugins: [forms, typography],
+};
+
+export default config;
